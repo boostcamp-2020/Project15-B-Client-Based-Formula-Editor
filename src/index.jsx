014e4787297111745e4ffc21a6266e7ab4c08ad2
woomin <dal96k@gmail.com>
2020-11-23 20:51:53 +0900

    init: redux 초기 환경 설정

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
+import {Provider} from "react-redux";
 
+import store from "./store";
 import App from "./App";
 
-ReactDom.render(<App />, document.getElementById("app"));
+ReactDom.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("app"),
+);
---
+++ src/slice.js
@@ -0,0 +1,17 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const {reducer, actions} = createSlice({
+	name: "FEditor",
+	initialState: {
+
+	},
+	reducers: {
+
+	},
+});
+
+export const {
+
+} = actions;
+
+export default reducer;
