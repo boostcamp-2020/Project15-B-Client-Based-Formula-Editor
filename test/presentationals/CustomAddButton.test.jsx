d28b7bcb82469bed317638911e49938c87c86118
woomin <dal96k@gmail.com>
2020-11-24 18:04:13 +0900

    feat: 사이드 바의 프레젠테이셔널 로직에 대해 테스트 작성
    사이드 바의 프레젠테이셔널들이 잘 렌더링되는지 확인하기 위해 테스트를
    작성함

---
+++ test/presentationals/CustomAddButton.test.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import CustomAddButton from "../../src/presentationals/CustomAddButton";
+
+describe("<CustomAddButton />", () => {
+	describe("with isFormOn state false", () => {
+		it("renders custom add button: default", () => {
+			const { container } = render(<CustomAddButton isFormOn={false} />);
+
+			expect(container).toHaveTextContent("새 커스텀 추가하기");
+		});
+	});
+
+	describe("with isFormOn state true", () => {
+		it("render custom add button: cancel ", () => {
+			const { container } = render(<CustomAddButton isFormOn={true} />);
+
+			expect(container).toHaveTextContent("취소");
+		});
+	});
+});
