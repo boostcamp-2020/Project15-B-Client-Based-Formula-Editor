c45145cfa03c42065ecfbffe1dadc74fe3772db5
enhakkore <ahn.ephraim@gmail.com>
2020-11-25 22:17:38 +0900

    feat: redo, undo 관련 reducer 수정
    - 의미없는 스프레드 연산자를 사용하여 지움.
    - redu, undo 관련 reducer를 export함.

--- src/slice.js
+++ src/slice.js
@@ -13,19 +13,22 @@ const { reducer, actions } = createSlice({
 			return { ...state, selectedButton: payload };
 		},
 		setPastLatexCommands(state, { payload }) {
-			return { ...state, pastLatexCommands: [...payload] };
+			return { ...state, pastLatexCommands: payload };
 		},
 		setPresentLatexCommand(state, { payload }) {
 			return { ...state, presentLatexCommand: payload };
 		},
 		setFutureLatexCommands(state, { payload }) {
-			return { ...state, futureLatexCommands: [...payload] };
+			return { ...state, futureLatexCommands: payload };
 		},
 	},
 });
 
 export const {
 	setSelectedButton,
+	setPastLatexCommands,
+	setPresentLatexCommand,
+	setFutureLatexCommands,
 } = actions;
 
 export default reducer;
