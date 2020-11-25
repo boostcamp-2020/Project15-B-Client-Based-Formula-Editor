ee495eaa207910888efd43915773c10b90497672
enhakkore <ahn.ephraim@gmail.com>
2020-11-25 22:30:59 +0900

    feat: undo 버튼 클릭 이벤트 등록
    undo 버튼을 클릭하면 이전 상태로 표시해주는 이벤트를 등록함.

--- src/containers/ControlButtonContainer.jsx
+++ src/containers/ControlButtonContainer.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
+import { setPastLatexCommands, setPresentLatexCommand, setFutureLatexCommands } from "../slice";
 import EditTabHeaderButton from "../presentationals/EditTabHeaderButton";
 
 export default function ControlButtonContainer(params) {
+	const dispatch = useDispatch();
+	const pastLatexCommands = useSelector(state => state.pastLatexCommands);
+	const presentLatexCommand = useSelector(state => state.presentLatexCommand);
+	const futureLatexCommands = useSelector(state => state.futureLatexCommands);
+
 	const handleUndoButton = () => {
-		console.log("clicked undo");
+		if (pastLatexCommands.length === 0) return;
+		const previousLatexCommand = pastLatexCommands[0];
+		const newPast = pastLatexCommands.slice(1);
+		const newFuture = [presentLatexCommand, ...futureLatexCommands];
+
+		dispatch(setFutureLatexCommands(newFuture));
+		dispatch(setPresentLatexCommand(previousLatexCommand));
+		dispatch(setPastLatexCommands(newPast));
 	};
+
 	const handleRedoButton = () => {
 		console.log("clicked redo");
 	};
