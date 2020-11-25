f36eca2e1689eeb2726a05a6f511c34f3cdb5397
enhakkore <ahn.ephraim@gmail.com>
2020-11-25 18:39:38 +0900

    feat: undo, redo, reset 관련 state와 reducer 추가
    수식 편집기에서 작성중인 latex command의 undo, redo, reset 기능을 위해 추가함

--- src/slice.js
+++ src/slice.js
@@ -4,10 +4,22 @@ const { reducer, actions } = createSlice({
 	name: "FEditor",
 	initialState: {
 		selectedButton: "",
+		pastLatexCommands: [],
+		presentLatexCommand: "",
+		futureLatexCommands: [],
 	},
 	reducers: {
 		setSelectedButton(state, { payload }) {
 			return { ...state, selectedButton: payload };
+		},
+		setPastLatexCommands(state, { payload }) {
+			return { ...state, pastLatexCommands: [...payload] };
+		},
+		setPresentLatexCommand(state, { payload }) {
+			return { ...state, presentLatexCommand: payload };
+		},
+		setFutureLatexCommands(state, { payload }) {
+			return { ...state, futureLatexCommands: [...payload] };
 		},
 	},
 });
