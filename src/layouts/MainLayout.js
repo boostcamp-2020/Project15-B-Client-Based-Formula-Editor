4e53c493d456edfdfffa1d027cc0744b3a6eef89
seokju2ng <ehddnwnd@naver.com>
2020-11-26 15:56:50 +0900

    refactor: 메인 페이지 width 반응형
    메인 페이지의 width가 500 ~ 1000px 까지 창 크기에 맞춰 변함

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -2,7 +2,14 @@ import styled from "styled-components";
 
 const MainLayout = styled.div`
   margin: 0 auto;
-  width: 1000px;
+  width: 100%;
+
+  @media(max-width: 500px) {
+    width: 500px;
+  }
+  @media(min-width: 1000px) {
+    width: 1000px;
+  }
 `;
 
 export default MainLayout;
