30de94f37ae62da9d801df5b2750c00599359ff3
woomin <dal96k@gmail.com>
2020-11-26 14:01:03 +0900

    chore: 수식/문자/연산자 버튼 색 변경 및 hover 지정

--- src/layouts/FormulaDropdownLayout.js
+++ src/layouts/FormulaDropdownLayout.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
+import { color } from "../GlobalStyle";
+
 const FormulaDropdownLayout = styled.div`
 	display: inline-block;
 	width: 260px;
 	height: 250px;
-	border: 1px solid black;
+	border: 1px solid ${color.normal};
 	padding: 8px;
-	background-color: white;
+	background-color: ${color.normal};
 	z-index: 5;
 `;
 
--- src/presentationals/FormulaButton.jsx
+++ src/presentationals/FormulaButton.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 
+import { color } from "../GlobalStyle";
+
 const FormulaButtonStyle = styled.button`
 	width: 80px;
 	height: 80px;
 	cursor: pointer;
-	& > svg {
+	background-color: ${color.light};
+	border: 1px solid ${color.dark};
+	outline: none;
+	
+	&:hover, &:focus {
+		background-color: ${color.normal};
+	}
+
+	> svg {
 		width: 50px;
 		height: 50px;
 	}
