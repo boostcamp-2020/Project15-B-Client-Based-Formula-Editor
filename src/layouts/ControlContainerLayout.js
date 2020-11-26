8b7838da219d368441cce5576c92aff44e46b1e0
seokju2ng <ehddnwnd@naver.com>
2020-11-27 06:30:03 +0900

    refactor: undo, redo, reset 버튼 아이콘으로 변경
    - undo, redo, reset 버튼에 아이콘 적용
    - 핸들링 함수 그룹화
    - EditTabHeaderButton이 필요없어서 삭제

---
+++ src/layouts/ControlContainerLayout.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const ControlContainerLayout = styled.div`
+  display: flex;
+  
+  > button {
+    margin-right: 5px;
+  }
+`;
+
+export default ControlContainerLayout;
