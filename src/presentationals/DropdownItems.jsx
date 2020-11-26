6e15c99a2d084ce9b26c400ba7e5dc6c02d6fcf3
woomin <dal96k@gmail.com>
2020-11-26 14:02:28 +0900

    chore: 레이아웃 로직을 프레젠테이션으로 이동
    이미 프레젠테이션인 파일인데 굳이 레이아웃 파일로부터 CSS를 불러올
    이유가 없다고 판단하여 그냥 프레젠테이션 컴포넌트 내부에 로직을 구현함

--- src/presentationals/DropdownItems.jsx
+++ src/presentationals/DropdownItems.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
-import FormulaDropdownLayout from "../layouts/FormulaDropdownLayout";
 import DropdownItem from "../presentationals/DropdownItem";
 import characterLatex from "../constants/characterLatex";
+import { color } from "../GlobalStyle";
+
+const FormulaDropdownLayout = styled.div`
+	display: inline-block;
+	width: 260px;
+	height: 250px;
+	border: 1px solid ${color.normal};
+	padding: 8px;
+	background-color: ${color.normal};
+	z-index: 5;
+`;
 
 export default function DropdownItems({ name, onItemClick }) {
 	return (
