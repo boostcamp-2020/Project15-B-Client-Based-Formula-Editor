2690df0f68a4ea098ec4f3b78b0fa50a025a0e52
enhakkore <ahn.ephraim@gmail.com>
2020-12-01 14:46:20 +0900

    feat: BookmarkContainer 수정
    - 로컬스토리지에서 북마크 아이템들이 들어있는 배열을 갖고 오도록 함.
    - 북마크 아이템을 클릭 이벤트 함수를 만들어 주입함.

--- src/containers/BookmarkContainer.jsx
+++ src/containers/BookmarkContainer.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 
+import { useDispatch } from "react-redux";
+import { setLatexInput } from "../slice";
+
 import ListLayout from "../layouts/ListLayout";
 import ListItem from "../presentationals/ListItem";
 import SideBarHeader from "../presentationals/SideBarHeader";
 import BookmarkAddButton from "../presentationals/BookmarkAddButton";
 
-export default function BookmarkContainer({ onScroll }) {
-	const bookmarkItems = [{ id: 0, latex: "2 + 3" }, { id: 1, latex: "4 + 5" }, { id: 2, latex: "6 + 7" }];
+
+export default function BookmarkContainer({ onScroll, setSidebar }) {
+	const dispatch = useDispatch();
+	const bookmarkItems = localStorage.getItem("bookmarkItems") || [];
 
 	const handleCustomButtonClick = () => {
 		console.log("custom");
 	};
 
+	const handleFormulaClick = latex => () => {
+		dispatch(setLatexInput(latex));
+		setSidebar(false);
+	};
+
 	return (
 		<ListLayout onScroll={onScroll}>
 			<SideBarHeader title={"북마크 수식 목록"} />
-			{bookmarkItems.map(({ id, latex }) =>
+			{bookmarkItems.map((latex, index) =>
 				<ListItem
-					key={id}
+					key={index}
 					latex={latex}
 					customOnClick={handleCustomButtonClick}
+					intoLatexFieldOnClick={handleFormulaClick(latex)}
 				/>)}
 			<BookmarkAddButton />
 		</ListLayout>
