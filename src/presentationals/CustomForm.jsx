51c4a168fec7a79c3aba4e31249afb25b6e91a69
enhakkore <ahn.ephraim@gmail.com>
2020-12-03 14:54:09 +0900

    feat: 커스텀 명령어 form의 name 필드에 자동 focus 기능 추가
    커스텀 명렁어 탭에서 등록 버튼을 클릭하면 name 필드에 자동으로 focus 되도록 함
    
    #29

--- src/presentationals/CustomForm.jsx
+++ src/presentationals/CustomForm.jsx
@@ -41,7 +41,7 @@ const WarningMsg = styled.p`
 export default function CustomForm({ data, onChangeCommand, onChangeLatex, onSubmit }) {
 	return (
 		<Form onSubmit={onSubmit}>
-			<input type="text" value={data.command} onChange={onChangeCommand} name="command" placeholder="명령어를 다음과 같이 입력하세요> \cmx" />
+			<input type="text" value={data.command} onChange={onChangeCommand} name="command" placeholder="명령어를 다음과 같이 입력하세요> \cmx" autoFocus/>
 			<WarningMsg isDisabled={data.isDisabled}>이미 있는 명령어입니다.</WarningMsg>
 			<EditableMathField latex={data.latex} onChange={onChangeLatex} name="latex" placeholder="mathquill 자리입니다." />
 			<Button name="submitBtn">{data.name}</Button>
