b63f3c19b4cf4a938b35827e7a1abf8770b7d3b7
seokju2ng <ehddnwnd@naver.com>
2020-12-03 17:24:32 +0900

    refactor: BodyContainer 중복되는 로직 제거

--- src/containers/BodyContainer.jsx
+++ src/containers/BodyContainer.jsx
@@ -22,12 +22,11 @@ export default function BodyContainer() {
 	} = useSelector(state => state);
 
 	const handleLatexInput = customList => mathField => {
-		const mathFieldLatex = mathField.latex();
+		let mathFieldLatex = mathField.latex();
 		const target = customList.find(elem => mathFieldLatex.includes(`#${elem.command}\\`));
 
 		if (target) {
-			dispatch(setLatexInputWithDebounce(mathFieldLatex.replace(`#${target.command}\\`, target.latex)));
-			return;
+			mathFieldLatex = mathFieldLatex.replace(`#${target.command}\\`, target.latex);
 		}
 		dispatch(setLatexInputWithDebounce(mathFieldLatex));
 	};
