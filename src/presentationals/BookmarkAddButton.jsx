9424cb0d480a310abf1531fdfa665dc37f042874
woomin <dal96k@gmail.com>
2020-12-03 17:12:35 +0900

    feat: 북마크 추가 버튼에 대한 CSS 적용

--- src/presentationals/BookmarkAddButton.jsx
+++ src/presentationals/BookmarkAddButton.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 
+import { color } from "../GlobalStyle";
+
 const Item = styled.button`
 	height: 150px;
 	position: relative;
-	background-color: lightgrey;
+	color: white;
+	font-weight: bold;
+	background-color: ${color.normal};
 	margin: 2.5px 5px;
 	border-radius: 15px;
-	border: 1px solid black;
+	border: 1px solid ${color.dark};
 	width: calc(100% - 10px); 
 	cursor: pointer;
 	outline: none;
 
 	&:hover {
-		background-color: grey;
+		background-color: ${color.dark};
 	} 
 `;
 
