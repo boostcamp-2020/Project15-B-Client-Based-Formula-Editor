8e1b0d87a083f17a94905d63b5576e67a3bcc032
enhakkore <ahn.ephraim@gmail.com>
2020-12-03 17:57:04 +0900

    style: GlobalStyle.js 들여쓰기 수정

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,18 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
+	* {
+		margin: 0;
+		padding: 0;
 		box-sizing: border-box;
-  }
-  
-  html, body, #app {
-    height: 100%;
-  }
+	}
+	
+	html, body, #app {
+		height: 100%;
+	}
 
 	body {
-    overflow-x: hidden;
+		overflow-x: hidden;
 	}
 `;
 
