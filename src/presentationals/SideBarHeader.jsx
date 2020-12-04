0d3dc134f7ed591bbf32e6a270c7abb38d8402e1
woomin <dal96k@gmail.com>
2020-12-04 11:09:41 +0900

    feat: 사이드바 탭 타이틀 옆에 전체삭제 버튼 추가
    매번 여러개를 각각 삭제하는 불편함이 있어서 전체 삭제 버튼 추가함

--- src/presentationals/SideBarHeader.jsx
+++ src/presentationals/SideBarHeader.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 
+import IconButton from "./IconButton";
+import CloseIcon from "../icons/CloseIcon";
+
+const Layout = styled.div`
+  display: flex;
+
+  > button {
+    display: none;
+  }
+
+  &:hover {
+    > button {
+      display: block;
+    }
+  }
+`;
+
 const Title = styled.div`
   text-align: center;
   margin: 15px 0;
   font-weight: 500;
 `;
 
-export default function SideBarHeader({ title }) {
-	return <Title>{title}</Title>;
+export default function SideBarHeader({ title, onClick }) {
+	return (
+		<Layout>
+			<Title>{title}</Title>
+			<IconButton
+				icon={<CloseIcon />}
+				isHover={true}
+				onClick={onClick}
+			/>
+		</Layout>
+	);
 }
