e01e1e656fd8e74cf9d52eb9876cd8dc2531e91a
enhakkore <ahn.ephraim@gmail.com>
2020-12-10 17:02:46 +0900

    feat: 자동완성 레이아웃 컴포넌트 추가
    커서 위치에 자동완성 레이아웃이 노출되는 컴포넌트

---
+++ src/presentationals/AutoComplete.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+import { StaticMathField } from "react-mathquill";
+
+import { themeColor } from "../GlobalStyle";
+
+const AutoKeywordLayout = styled.div`
+	position: fixed;
+	left: ${(({ x }) => x)}px;
+	top: ${(({ y }) => y + 15)}px;
+	width: fit-content;
+	height: fit-content;
+	background: ${themeColor.normal};
+	color: white;
+	box-shadow: 0 0 7px 3px black;
+	visibility: ${({ isOpen }) => (isOpen ? "visible" : "hidden")};
+`;
+
+const HightLight = styled.div`
+	display: flex;
+	min-width: 180px;
+	padding: 2px 5px;
+
+	${(({ isFocused }) => {
+		if (isFocused) {
+			return `
+				background: #5F7E97;
+				color: white;`;
+		}
+
+		return ``;
+	})}
+`;
+
+const ItemWrapper = styled.div`
+  min-width: 100px;
+	margin-right: auto;
+`;
+
+const MathFieldWrapper = styled.div`
+	margin-left: auto;
+`;
+
+export default function AutoComplete({
+	isOpen,
+	x,
+	y,
+	recommandationList,
+	targetIndex,
+}) {
+	return (
+		<AutoKeywordLayout x={x} y={y} isOpen={isOpen}>
+			{ isOpen && recommandationList.map((item, index) => (
+				<HightLight key={index} isFocused={ index === targetIndex}>
+					<ItemWrapper>{item}</ItemWrapper>
+					<MathFieldWrapper>
+						<StaticMathField key={`S${item}`}>{item}</StaticMathField>
+					</MathFieldWrapper>
+				</HightLight>
+			))}
+		</AutoKeywordLayout>
+	);
+}
