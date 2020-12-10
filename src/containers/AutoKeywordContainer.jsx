f760c551792d56ddd78dfe34c5da49984901b52f
woomin <dal96k@gmail.com>
2020-12-10 13:38:52 +0900

    feat: 자동완성 이벤트를 갖는 컴포넌트 구현
    사용자의 키보드 이벤트를 감지해서 자동완성을 시켜줄 수 있는 인터페이스를
    제공하는 컴포넌트를 구현함

---
+++ src/containers/AutoKeywordContainer.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+
+import { latexFunction } from "../util";
+import KEY_CODE from "../constants/keyCode";
+
+export default function AutoKeywordContainer() {
+	const cursorPosition = useSelector(state => state.cursorPosition);
+	const latexInput = useSelector(state => state.latexInput);
+	const [isOpen, toggleIsOpen] = useState(false);
+	const [itemIndex, setItemIndex] = useState(0);
+	const [backslashCount, setBackslashCount] = useState(0);
+	const buffer = useRef([]);
+	const [recommandationList, setRecommandationList] = useState([]);
+
+	const keyupEvent = ({ keyCode }) => {
+		if (keyCode === KEY_CODE.BACK_SLASH) {
+			setBackslashCount(backslashCount + 1);
+			toggleIsOpen(!isOpen);
+		}
+
+		if (isOpen && keyCode === KEY_CODE.BACK_SPACE) {
+			const backslashCountInLatex = latexInput.split("").filter(char => char === "\\").length;
+
+			buffer.current.pop();
+
+			if (backslashCountInLatex !== backslashCount) {
+				setBackslashCount(backslashCount - 1);
+				toggleIsOpen(false);
+			}
+		}
+	};
+
+	const keydownEvent = ({ keyCode }) => {
+		if (!isOpen) return;
+
+		if (keyCode === KEY_CODE.DOWN) {
+			const nextIndex = (itemIndex + 1) % recommandationList.length;
+
+			setItemIndex(nextIndex);
+		}
+
+		if (keyCode === KEY_CODE.UP) {
+			const prevIndex = (itemIndex - 1) < 0 ? recommandationList.length - 1 : itemIndex - 1;
+
+			setItemIndex(prevIndex);
+		}
+
+		if (keyCode === KEY_CODE.ENTER || keyCode === KEY_CODE.SPACE || keyCode === KEY_CODE.TAB) {
+			const target = recommandationList[itemIndex].title;
+
+			const temp = buffer.current.join("").trim()
+				.toLowerCase();
+
+			const remainedLatexPart = target.replace(`\\${temp}`, "");
+
+			latexFunction.insertLatex(remainedLatexPart);
+
+			buffer.current = [];
+			toggleIsOpen(false);
+			setItemIndex(0);
+		}
+	};
+
+	const keypressEvent = ({ keyCode }) => {
+		if (!isOpen) return;
+
+		const isUpperAlphabet = keyCode >= 65 && keyCode <= 90;
+		const isLowerAlphabet = keyCode >= 97 && keyCode <= 122;
+
+		if (isLowerAlphabet || isUpperAlphabet) {
+			const character = String.fromCharCode(keyCode);
+
+			buffer.current.push(character);
+		}
+
+		if (keyCode === KEY_CODE.SPACE) {
+			toggleIsOpen(false);
+		}
+	};
+
+	useEffect(() => {
+		const rootBlock = document.querySelector(".mq-textarea");
+
+		rootBlock?.addEventListener("keypress", keypressEvent);
+		rootBlock?.addEventListener("keydown", keydownEvent);
+		rootBlock?.addEventListener("keyup", keyupEvent);
+
+		return () => {
+			rootBlock?.removeEventListener("keypress", keypressEvent);
+			rootBlock?.removeEventListener("keydown", keydownEvent);
+			rootBlock?.removeEventListener("keyup", keyupEvent);
+		};
+	});
+
+	return (
+		<div // 여기에 자동완성 박스
+			isOpen={isOpen}
+			x={cursorPosition.x}
+			y={cursorPosition.y}
+			recommandationList={recommandationList}
+			targetIndex={itemIndex}
+		/>
+	);
+}
