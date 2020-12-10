a3996f7c94e6c18b2f7b2cd05b50ea28fea3514b
woomin <dal96k@gmail.com>
2020-12-11 02:03:50 +0900

    chore: babel optional chaining 설치

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,7 @@ module.exports = function(api) {
 		], "@babel/preset-react",
 	];
 
-	const plugins = [];
+	const plugins = ["@babel/plugin-proposal-optional-chaining"];
 
 	return { presets, plugins };
 };
