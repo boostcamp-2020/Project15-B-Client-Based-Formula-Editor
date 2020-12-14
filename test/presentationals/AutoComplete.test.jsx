2a5aa82487e8beb77c7fcab1dec66df9f6a8ecb8
woomin <dal96k@gmail.com>
2020-12-14 13:26:51 +0900

    test: BlueButton에 대한 테스트 작성

--- test/presentationals/AutoComplete.test.jsx
+++ test/presentationals/AutoComplete.test.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { render } from "@testing-library/react";
-
 import "@testing-library/jest-dom";
 
 import AutoComplete from "../../src/presentationals/AutoComplete";
