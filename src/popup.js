5d328dd9fbbfe52f36b53cba2bb77656e4be5cea
woomin <dal96k@gmail.com>
2020-12-14 18:37:35 +0900

    fix: input이 존재하지 않을때 발생하는 에러 수정
    mode가 confirm일 때는 input이 없어서 undefined인데, 여기에다가 focus
    이벤트를 주게되어 에러가 발생하는 문제가 있어서 이를 해결함

--- src/popup.js
+++ src/popup.js
@@ -66,7 +66,7 @@ const customPopup = ({ mode, message }) => new Promise(resolve => {
 	};
 
 	document.body.appendChild(popup);
-	input.focus();
+	input?.focus();
 });
 
 export default customPopup;
