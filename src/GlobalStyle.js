73b707a1ec2eef256289a5ca38ca9cc6b4fbb97c
woomin <dal96k@gmail.com>
2020-12-20 01:33:24 +0900

    fix: NotoSans 폰트가 수식에도 적용되는 문제 해결

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -34,7 +34,10 @@ const GlobalStyle = createGlobalStyle`
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
-		font-family: "NotoSansKR-Regular";
+
+		&:not(.mq-math-mode, .mq-editable-field) {
+			font-family: "NotoSansKR-Regular";
+		}
 	}
 	
 	html, body, #app {
